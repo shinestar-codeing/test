e1c900ca189cbe6e5d77617085cf966c38ccd35a
shinestar-codeing <66462962+shinestar-codeing@users.noreply.github.com>
2022-07-31 09:27:12 -0700

    Update camara_script.cs

--- scripts/camara_script.cs
+++ scripts/camara_script.cs
@@ -10,7 +10,7 @@ public class camara_script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //sfhashjarhjsdarjsra
     }
 
     // Update is called once per frame
